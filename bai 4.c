7fac256fe2be757e8d1ef1e948edfb1cc15c0a6a
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-11-27 17:23:42 +0700

    bai4

---
+++ bai 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+	int a;
+	int a[n];
+	printf("Nhap so phan tu cua mang:");
+	scanf("%d", &a);
+	
+	
+	if (a<=0) {
+		printf("So phan tu khong hop le,nhap lai phan tu\n");
+		return 1; 
+	}
+}
